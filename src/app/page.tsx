d7e2bed832b066cc9865b2397238ac25e5e9f34a
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2026-05-25 09:50:48 -0300

    Adicionando componente de seção na página para testes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import styles from "./page.module.css";
 
+import { Section } from "@/components/shared/section/section";
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Section><h1>Seção 1</h1></Section>
+        <Section><h1>Seção 2</h1></Section>
+        <Section><h1>Seção 3</h1></Section>
+        <Section><h1>Seção 4</h1></Section>
+        <Section><h1>Seção 5</h1></Section>
       </main>
     </div>
   );
