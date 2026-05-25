b99b42f850710b84c6c46d3a0017ade68a271859
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2026-05-25 10:29:01 -0300

    Incluindo os componentes de vídeo para testes e identando o código. Acrescentando também algumas variáveis globais de estilo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import styles from "./page.module.css";
 
-import { Section } from "@/components/shared/section/section";
+import { Movies } from "@/components/UI/movies/Movies";
+import { Section } from "@/components/shared/section/Section";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Section><h1>Seção 1</h1></Section>
+        <Section><Movies src=""/></Section>
         <Section><h1>Seção 2</h1></Section>
         <Section><h1>Seção 3</h1></Section>
         <Section><h1>Seção 4</h1></Section>
