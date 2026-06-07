d3f97a968150ea0fe49579ec318c456befc21229
Hypperzin <hypperzin@Hypperzin.Hypperzin>
2026-06-07 19:07:46 -0300

    Adicionando o componente na seção para testes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import styles from "./page.module.css";
 
 import { Movies } from "@/components/UI/movies/Movies";
 import { Section } from "@/components/shared/section/Section";
+import { Smartphone } from "@/components/UI/cards/card_smarphones/Smartphone";
 
 import Title from "@/components/UI/title/Title";
 import Confirm_button from "@/components/UI/buttons/confirm/Confirm_button";
@@ -31,8 +32,17 @@ export default function Home() {
             </div>
           </Movies>
         </Section>
-        <Section data_type={2}>
-          <h1>Seção 2</h1>
+
+        <Section id={styles.media_section} data_type={1}>
+          <Title class={styles.title_medias}>Veja o dia a dia de quem estuda no Orion !</Title>
+          <div className={styles.content_section_media}>
+            <Smartphone src="/medias/instagram_2.png"/>
+            <Smartphone src="/medias/instagram_center.png" id={styles.media_center}/>
+            <Smartphone src="/medias/instagram_1.png"/>
+            <button className={styles.media_button_section}>
+              <img className={styles.media_button_icon} src="/icons/arrow_icon.svg"/>
+            </button>
+          </div>
         </Section>
         <Section data_type={1}>
           <h1>Seção 3</h1>
